5ebd0a57f18fd4fd6718ebdb30b03a778f6cdc26
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-22 16:58:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,6 +15,6 @@ end
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
   hash_2 = {}
-  hash_2[:key] = number
+  hash_2[:id] = number
   p hash_2
 end
